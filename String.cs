66eed22bb1872b593efab2e9a124fb3f1671507d
CodeWithPartha <153088732+CodeWithPartha@users.noreply.github.com>
2023-12-06 20:56:52 -0800

    Partha

---
+++ String.cs
@@ -0,0 +1,26 @@
+using System;
+class Program{
+    static void Main(String[] args) {
+        
+/*        String fullName="Partha";
+        String fullName1="Partha";
+        fullName = fullName.ToUpper();
+        fullName1 = fullName1.ToLower();
+        Console.WriteLine(fullName);
+        Console.WriteLine(fullName1);*/
+        
+/*        String phoneNumber = "123-456-655-333";
+        phoneNumber = phoneNumber.Replace("-", "/");
+        Console.WriteLine(phoneNumber);*/
+        
+/*        String fullName="Partha";
+        //String userName =fullName.Insert(0, "@");
+       String userName =fullName.Insert(0, "Mr. ");
+       String userName1 =fullName.Insert(6, " Rakshit ");
+       String firstName= fullName.Substring(0, 3);
+        Console.WriteLine(userName);
+        Console.WriteLine(userName1);
+        Console.WriteLine(userName1.Length);
+        Console.WriteLine(userName1.Length);*/
+    }
+}
